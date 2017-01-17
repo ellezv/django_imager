64793a6d03ccb266238956c68a4e582aa9b296ab
Julien <julienawilson@gmail.com>
2017-01-16 18:03:08 -0800

    testing for user and imagerprofile

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,31 @@
+"""Tests for Imager Site."""
 from django.test import TestCase
+from django.contrib.auth.models import User
+from imager_profile.models import ImagerProfile
 
-# Create your tests here.
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    username = factory.Sequence(lambda n: "Imgr User {}".format(n))
+    email = factory.LazyAttribute(
+        lambda x: "{}@datsite.com".format(x.username.replace(" ", ""))
+    )
+
+
+class ImagerProfileCase(TestCase):
+    """The Profile Model Test Runner."""
+
+    def setUp(self):
+        """DB setup for tests."""
+        self.users = [UserFactory.create() for i in range(10)]
+
+    def test_profile_made_when_user_saved(self):
+        """Test that making a user also makes a profile."""
+        self.assertTrue(ImagerProfile.objects.count() == 10)
+
+    def test_profile_same_data_as_user(self):
+        uid = User.objects.first().id
+        self.assertTrue(ImagerProfile.objects.filter(id=uid).first().user.username == User.objects.first().username)
