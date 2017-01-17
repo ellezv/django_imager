53ec12c8f3c92a317b7d03ffc91d73028179da50
Julien <julienawilson@gmail.com>
2017-01-16 17:20:44 -0800

    begun imagerprofile class

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+from django.db.models.signals import post_save
+from django.dispath import receiver
+
 
 # Create your models here.
+
+class ImagerProfile(models.Model):
+    """Profile class for all imager users."""
+
+    user = models.OneToOneField(
+        User,
+        related_name="profile",
+        on_delete=models.CASCADE
+    )
+    address = models.CharField(max_length=255, blank=True, null=True)
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    website = models.CharField(max_length=255, blank=True, null=True)
+    hireable = models.BooleanField(default=True)
+    travel_radius = models.IntegerField(default=100)
+    phone_number = models.CharField(max_length=10)
+    camera_type = models.CharField(max_length=255, blank=True, null=True)
+    photography_type = models.Choices(
+        ('LA', 'Landscape'),
+        ('WD', 'Weddings'),
+        ('AN', 'Animals'),
+        ('PT', 'Portraits'),
+        ('UR', 'Urban'),
+        ('FA', 'Fashion'),
+        ('TR', 'Travel'),
+        ('WA', 'Water')
+    )
