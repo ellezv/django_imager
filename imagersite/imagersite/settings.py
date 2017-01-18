6a7522a5839193b48ce0a4d83531427acef67ec5
Maelle Vance <maellevance@gmail.com>
2017-01-18 10:31:10 -0800

    login view functional

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'imager_profile',
+    'imagersite'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
