222e7fc25fe89076d2f23e5c57538ae288e49f9e
Julien <julienawilson@gmail.com>
2017-01-18 16:46:48 -0800

    changes from class

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from imagersite.views import home_view
 from django.contrib.auth import views as auth_views
@@ -24,6 +24,5 @@ urlpatterns = [
     url(r'^$', home_view, name='home'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-
-
+    url(r'^registration/', include("reistration.backends.hmac.urls"))
 ]
