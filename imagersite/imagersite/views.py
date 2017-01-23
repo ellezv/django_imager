06586c515f9fc2c318fa50191cd0fe3752d053d5
Maelle Vance <maellevance@gmail.com>
2017-01-22 19:03:42 -0800

    random background for homepage

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -6,11 +6,13 @@ from imager_images.models import Image, Album
 
 def home_view(request):
     """Home view for the imager site."""
-    ps_rand_image = Image.objects.all()[1].image.url
+    import random
+    img_lst = Image.objects.all()
+    img = random.choice(img_lst)
     return render(
         request,
         'imagersite/home.html',
-        {'static_background': ps_rand_image}
+        {'static_background': img.image.url}
     )
 # note: when our database has images, we can query it and return a random path
 # as the static background.
