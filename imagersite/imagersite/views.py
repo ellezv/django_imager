b73b819b89cd207b021131986bf5e34b73ff0451
Maelle Vance <maellevance@gmail.com>
2017-01-23 20:16:07 -0800

    stock photo background if nothing in DB

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,16 +1,20 @@
 """Views for Imager Site."""
 
 from django.shortcuts import render
-from imager_images.models import Image, Album
+from imager_images.models import Image
+from django.conf import settings
 
 
 def home_view(request):
     """Home view for the imager site."""
     import random
     img_lst = Image.objects.all()
-    img = random.choice(img_lst)
+    if not len(img_lst):
+        img_url = settings.MEDIA_URL + "banff.jpg"
+    else:
+        img_url = random.choice(img_lst).image.url
     return render(
         request,
         'imagersite/home.html',
-        {'static_background': img.image.url}
+        {'static_background': img_url}
     )
