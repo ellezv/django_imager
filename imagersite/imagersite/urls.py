54ca3c7e336f5047781a6dd4f1edecbcac3b5727
Maelle Vance <maellevance@gmail.com>
2017-01-24 17:07:34 -0800

    class based HomeView and url updated

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from imagersite.views import home_view
+from imagersite.views import HomeView
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home_view, name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^registration/', include("registration.backends.hmac.urls")),
--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -3,13 +3,29 @@
 from django.shortcuts import render
 from imager_images.models import Image
 from django.conf import settings
+from django.views.generic import TemplateView
+import random
+
+
+class HomeView(TemplateView):
+    """A class based view for home page."""
+
+    template_name = "imagersite/home.html"
+
+    def get_context_data(self):
+        """Extending get_context_data method."""
+        img_lst = Image.objects.all()
+        if not len(img_lst):
+            static_background = settings.STATIC_URL + "banff.jpg"
+        else:
+            static_background = random.choice(img_lst).image.url
+        return {"static_background": static_background}
 
 
 def home_view(request):
     """Home view for the imager site."""
     import random
     img_lst = Image.objects.all()
-    import pdb; pdb.set_trace()
     if not len(img_lst):
         img_url = settings.STATIC_URL + "banff.jpg"
     else:
