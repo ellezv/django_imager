662514eee5c1a4e62f16e456452e17847fb1975b
Maelle Vance <maellevance@gmail.com>
2017-01-24 17:09:03 -0800

    docstring, deleted corpse code, reorganized import on imagersite views

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,9 +1,10 @@
 """Views for Imager Site."""
 
-from django.shortcuts import render
-from imager_images.models import Image
 from django.conf import settings
 from django.views.generic import TemplateView
+
+from imager_images.models import Image
+
 import random
 
 
@@ -13,25 +14,10 @@ class HomeView(TemplateView):
     template_name = "imagersite/home.html"
 
     def get_context_data(self):
-        """Extending get_context_data method."""
+        """Extending get_context_data method to return the data we need."""
         img_lst = Image.objects.all()
         if not len(img_lst):
             static_background = settings.STATIC_URL + "banff.jpg"
         else:
             static_background = random.choice(img_lst).image.url
         return {"static_background": static_background}
-
-
-def home_view(request):
-    """Home view for the imager site."""
-    import random
-    img_lst = Image.objects.all()
-    if not len(img_lst):
-        img_url = settings.STATIC_URL + "banff.jpg"
-    else:
-        img_url = random.choice(img_lst).image.url
-    return render(
-        request,
-        'imagersite/home.html',
-        {'static_background': img_url}
-    )
