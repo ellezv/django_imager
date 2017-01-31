5e353eaea6028dab02dbdd514a351d1e56b72791
Julien <julienawilson@gmail.com>
2017-01-31 09:15:35 -0800

    settings update

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static', 'vendor')]
 STATIC_ROOT = BASE_DIR
 STATIC_URL = '/static/'
 
