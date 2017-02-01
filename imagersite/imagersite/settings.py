c4f56702f604b302b302a7beb12705243a84790d
Julien <julienawilson@gmail.com>
2017-02-01 14:32:45 -0800

    replace static root

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static', 'vendor')]
-STATIC_ROOT = BASE_DIR
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static', 'vendor')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
