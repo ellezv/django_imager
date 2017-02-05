c77f8528d7097305bd8b47838c8d058262b0cfdd
Maelle Vance <maellevance@gmail.com>
2017-02-04 16:13:36 -0800

    started on views

--- imagersite/api/views.py
+++ imagersite/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from imager_images.models import Image
+from api.serializers import ImageSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-# Create your views here.
+
+class ImageList(mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                generics.GenericAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class ImageDetail(mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin,
+                generics.GenericAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
